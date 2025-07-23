0ffdb04ae16ea66bae5fab73090ab204922659f9
Jerrybery <jerryl01@qq.com>
2025-07-23 09:12:16 -0400

    remove client_tts.py

--- client_tts.py
+++
@@ -1,7 +0,0 @@
-from piper import PiperVoice
-import wave
-
-
-voice = PiperVoice.load("en_US-lessac-medium.onnx")
-with wave.open("test.wav", "wb") as wav_file:
-    voice.synthesize_wav("Welcome to the world of speech synthesis!", wav_file)
